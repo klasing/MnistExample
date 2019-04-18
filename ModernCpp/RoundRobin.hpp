8764ff622f33391d953f97826aff267bb13b9055
klasing <klasing1959@gmail.com>
2019-04-19 00:55:56 +0200

    19-04-2019 00:55

--- ModernCpp/RoundRobin.hpp
+++ ModernCpp/RoundRobin.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 // Class template RoundRobin
 // Provides simple round-robin semantics for a list of elements.
 template <typename T>
@@ -7,4 +9,85 @@ class RoundRobin {
 public:
 	// Client can give a hint as to the number of expected elements for
 	// increased efficiency.
+	RoundRobin(int numExpected = 0) {
+		// If the client gave a guideline, reserve that much space.
+		mElems.reserve(numExpected);
+		// Initialize mCurElem even though it isn't used until
+		// there's at least one element.
+		mCurElem = mElems.begin();
+	}
+
+	virtual ~RoundRobin() {
+		// nothing to de here -- the vector will delete all the elements
+	}
+
+	// Appends elem to the end of the list. May be called
+	// between calls to  getNext().
+	void add(const T& elem) {
+		// Even though we add the element at the end, the vector could
+		// reallocate and invalidate the iterator with the push_back call.
+		// Take advantage of the random access iterator features to save our
+		// spot.
+		int pos = mCurElem - mElems.begin();
+		// add the element
+		mElems.push_back(elem);
+		// Reset our iterator to make sure it is valid.
+		mCurElem = mElems.begin() + pos;
+	}
+
+	// Removes the first (and only the first) element
+	// in the list that is equal (with operator==) to elem.
+	// May be called between calls to getNext().
+	void remove(const T& elem) {
+		for (auto it = mElems.begin(); it != mElems.end(); ++it)
+			if (*it == elem) {
+				// Removing an element will invalidate our mCurElem iterator if
+				// it refers to an element past the point of the removal.
+				// Take advantage of the random access features of the iterator
+				// to track the position of the current element after removal.
+				int newPos;
+				// if current iterator is before or at the one we're removing,
+				// the new position is the same as before.
+				if (mCurElem <= it)
+					newPos = mCurElem - mElems.begin();
+				else
+					// otherwise, it's one less than before
+					newPos = mCurElem - mElems.begin() - 1;
+				// erase the element (and ignore the return value)
+				mElems.erase(it);
+				// Now reset our iterator to make sure it is valid.
+				mCurElem = mElems.begin() + newPos;
+				// If we were pointing to the last element and it was removed,
+				// we need to loop back to the first.
+				if (mCurElem == mElems.end())
+					mCurElem = mElems.begin();
+				return;
+			}
+	}
+	// Returns the next element in the list, starting with the first,
+	// and cycling back to the first when the end of the list is
+	// reached, taking into account elements that are added or removed.
+	T& getNext() throw(out_of_range) {
+		// First make sure there are any elements.
+		if (mElems.empty())
+			throw out_of_range("No elements in the list");
+		// retrieve a reference to return
+		T& retVal = *mCurElem;
+		// Increment the iterator modulo the number of elements
+		++mCurElem;
+		if (mCurElem == mElems.end())
+			mCurElem = mElems.begin();
+		// return the reference
+		return retVal;
+	}
+protected:
+	vector<T> mElems;
+	typename vector<T>::iterator mCurElem;
+private:
+	// Prevent assignment and pass by value
+	RoundRobin(const RoundRobin& src);
+	RoundRobin& operator=(const RoundRobin& rhs) {
+		// not according to text in book
+		return *this;
+	}
 };
--- ModernCpp/SubParagraph_12_2_1.cpp
+++ ModernCpp/SubParagraph_12_2_1.cpp
@@ -1,16 +1,128 @@
 #include "pch.h"
 
+#include <string>
+#include "RoundRobin.hpp"
+
+// Simple Process class
+class Process {
+public:
+	// Constructor accepting the name of the process.
+	Process(const string& name) : mName(name) {}
+	// Implementation of doWorkDuringTimeSlice would let the process
+	// perform its work for the duration of a time slice.
+	// Actual implementation omtitted.
+	void doWorkDuringTimeSlice() {
+		cout << "-> Process " << mName
+			<< " performing work during time slice." << endl;
+	}
+	// Needed for the RoundRobin::remove method to work.
+	bool operator==(const Process& rhs) {
+		return mName == rhs.mName;
+	}
+protected:
+	string mName;
+};
+
+// Simple round-robin based process scheduler.
+class Scheduler {
+public:
+	// Constructor takes a vector of processes.
+	Scheduler(const vector<Process>& processes) {
+		// Add the processes
+		for (auto& process : processes)
+			rr.add(process);
+	}
+	// Selects the next process using a round-robin scheduling
+	// algorithm and allows it to perform some work during
+	// this time slice.
+	void scheduleTimeSlice() {
+		try {
+			rr.getNext().doWorkDuringTimeSlice();
+		}
+		catch (const out_of_range&) {
+			cerr << "-> No more processes to schedule." << endl;
+		}
+	}
+	// Removes the given process from the list of processes.
+	void removeProcess(const Process& process) {
+		rr.remove(process);
+	}
+protected:
+	RoundRobin<Process> rr;
+};
+
 inline void subParagraph_12_2_1() {
 	cout << "vector" << endl;
 	cout << "------" << endl;
 
 	cout << "a) vector Overview" << endl;
 	cout << "b) Fixed-Length vectors" << endl;
+	// Create a vector of 10 doubles
+	vector<double> doubleVector(10);
+	// Initialize max to smallest number
+	double max = numeric_limits<double>::lowest();
+	for (size_t i = 0; i < 10; i++)
+		doubleVector[i] = .0;
+	for (size_t i = 0; i < 10; i++) {
+		cout << "-> Enter score " << i + 1 << ": ";
+		cin >> doubleVector[i];
+		if (doubleVector[i] > max)
+			max = doubleVector[i];
+	}
+	max /= 100.0;
+	cout << "-> ";
+	for (size_t i = 0; i < 10; i++) {
+		doubleVector[i] /= max;
+		cout << doubleVector[i] << " ";
+	}
+	cout << endl;
 	cout << "   b.1) Specifying an Initial Element Value" << endl;
+	// 10 doubles of value 0.0
+	vector<double> doubleVector_(10, .0);
+	// Initialize max to smallest number
+	double max_ = numeric_limits<double>::lowest();
+	// No need to initialize each element: the constructor did it for you
+	//for (size_t i = 0; i < 10; i++)
+	//	doubleVector_[i] = .0;
+	for (size_t i = 0; i < 10; i++) {
+		cout << "-> Enter score " << i + 1 << ": ";
+		cin >> doubleVector_[i];
+		if (doubleVector_[i] > max_)
+			max_ = doubleVector_[i];
+	}
+	max_ /= 100.0;
+	cout << "-> ";
+	for (size_t i = 0; i < 10; i++) {
+		doubleVector_[i] /= max_;
+		cout << doubleVector_[i] << " ";
+	}
+	cout << endl;
 	cout << "   b.2) Other vector Element Access Methods" << endl;
 	cout << "c) Dynamic-Length vectors" << endl;
+	// Create a vector with zero elements
+	vector<double> doubleVector_ex;
+	// Initialize max to smallest number
+	double max_ex = numeric_limits<double>::lowest();
+	for (size_t i = 0; i < 10; i++) {
+		double temp;
+		cout << "-> Enter score " << i + 1 << "(-1 to stop): ";
+		cin >> temp;
+		if (temp == -1)
+			break;
+		doubleVector_ex.push_back(temp);
+		if (temp < max_ex)
+			max_ex = temp;
+	}	
+	max_ex /= 100.0;
+	cout << "-> ";
+	for (size_t i = 0; i < doubleVector_ex.size(); i++) {
+		doubleVector_ex[i] /= max_ex;
+		cout << doubleVector_ex[i] << " ";
+	}
+	cout << endl;
 	cout << "d) vector Details" << endl;
 	cout << "   d.1) Constructors and Destructors" << endl;
+
 	cout << "e) Copying and Assigning vectors" << endl;
 	cout << "f) Comparing vectors" << endl;
 	cout << "g) vector Iterators" << endl;
@@ -20,9 +132,18 @@ inline void subParagraph_12_2_1() {
 	cout << "   h.3) Other Iterator Operations" << endl;
 	cout << "   h.4) Iterators versus Indexing" << endl;
 	cout << "i) Adding and Removing Elements" << endl;
+
 	cout << "   i.1) Algorithmic Complexity and Iterator Invalidation" << endl;
 	cout << "j) The vector Memory Allocation Scheme" << endl;
 	cout << "   j.1) Size and Capacity" << endl;
 	cout << "   j.1) Reserving Capacity" << endl;
 	cout << "k) vector Example: A Round-Robin Class" << endl;
+	vector<Process> processes = { Process("1"), Process("2"), Process("3") };
+	Scheduler sched(processes);
+	for (int i = 0; i < 4; ++i)
+		sched.scheduleTimeSlice();
+	sched.removeProcess(processes[1]);
+	cout << "-> Removed second process" << endl;
+	for (int i = 0; i < 4; ++i)
+		sched.scheduleTimeSlice();
 }
