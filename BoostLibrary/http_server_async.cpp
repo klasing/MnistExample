937d59d55fb7f09f08f7bca581c8e94494b6ec6e
klasing <klasing1959@gmail.com>
2019-06-16 11:57:53 +0200

    16-06-2019 11:57

--- BoostLibrary/http_server_async.cpp
+++ BoostLibrary/http_server_async.cpp
@@ -25,6 +25,9 @@ namespace net = boost::asio;
 
 using tcp = boost::asio::ip::tcp;
 namespace ns_http_server_async {
+	//***************************************************************************
+	//*                    mime_type
+	//***************************************************************************
 	// Return a reasonable mime type based on the extension of a file.
 	inline beast::string_view mime_type(beast::string_view path) {
 		using beast::iequals;
@@ -58,6 +61,9 @@ namespace ns_http_server_async {
 		return "application/text";
 	}
 
+	//***************************************************************************
+	//*                    path_cat
+	//***************************************************************************
 	// Append an HTTP rel-path to a local filesystem path.
 	// The returned path is normalized for the platform.
 	inline std::string path_cat(beast::string_view base, beast::string_view path) {
@@ -81,6 +87,9 @@ namespace ns_http_server_async {
 		return result;
 	}
 
+	//***************************************************************************
+	//*                    handle_request
+	//***************************************************************************
 	// This function produces an HTTP response for the given
 	// request. The type of the response object depends on the
 	// contents of the request, so the interface requires the
@@ -178,11 +187,17 @@ namespace ns_http_server_async {
 
 	//---------------------------------------------------------------------------
 
+	//***************************************************************************
+	//*                    fail
+	//***************************************************************************
 	// Report a failure
 	inline void fail(beast::error_code ec, char const* what) {
 		std::cerr << what << ": " << ec.message() << '\n';
 	}
 
+	//***************************************************************************
+	//*                    session
+	//***************************************************************************
 	// Handles an HTTP server connection
 	class session : public std::enable_shared_from_this<session> {
 		// This is the C++ equivalent of a generic lambda.
@@ -300,86 +315,92 @@ namespace ns_http_server_async {
 		}
 	};
 
-		//--------------------------------------------------------------------------
+	//--------------------------------------------------------------------------
 
-		// Accepts incoming connections and launches the sessions
-		class listener : public std::enable_shared_from_this<listener> {
-			net::io_context& ioc_;
-			tcp::acceptor acceptor_;
-			std::shared_ptr<std::string const> doc_root_;
+	//***************************************************************************
+	//*                    listener
+	//***************************************************************************
+	// Accepts incoming connections and launches the sessions
+	class listener : public std::enable_shared_from_this<listener> {
+		net::io_context& ioc_;
+		tcp::acceptor acceptor_;
+		std::shared_ptr<std::string const> doc_root_;
 
-		public:
-			listener(
-				net::io_context& ioc,
-				tcp::endpoint endpoint,
-				std::shared_ptr<std::string const> const& doc_root) 
-				: ioc_(ioc)
-				, acceptor_(net::make_strand(ioc))
-				, doc_root_(doc_root) {
-				cout << "<<constructor>> listener()" << endl;
+	public:
+		listener(
+			net::io_context& ioc,
+			tcp::endpoint endpoint,
+			std::shared_ptr<std::string const> const& doc_root)
+			: ioc_(ioc)
+			, acceptor_(net::make_strand(ioc))
+			, doc_root_(doc_root) {
+			cout << "<<constructor>> listener()" << endl;
 
-				beast::error_code ec;
+			beast::error_code ec;
 
-				// Open the acceptor
-				acceptor_.open(endpoint.protocol(), ec);
-				if (ec) {
-					fail(ec, "open");
-					return;
-				}
-
-				// Allow address reuse
-				acceptor_.set_option(net::socket_base::reuse_address(true), ec);
-				if (ec) {
-					fail(ec, "set_option");
-					return;
-				}
-
-				// Bind to the server address
-				acceptor_.bind(endpoint, ec);
-				if (ec) {
-					fail(ec, "bind");
-					return;
-				}
-
-				// Start listening for connections
-				acceptor_.listen(net::socket_base::max_listen_connections, ec);
-				if (ec) {
-					fail(ec, "listen");
-					return;
-				}
+			// Open the acceptor
+			acceptor_.open(endpoint.protocol(), ec);
+			if (ec) {
+				fail(ec, "open");
+				return;
 			}
 
-			// Start accepting incoming connections
-			void run() {
-				do_accept();
+			// Allow address reuse
+			acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+			if (ec) {
+				fail(ec, "set_option");
+				return;
 			}
 
-		private:
-			void do_accept() {
-				// The new connection gets its own strand
-				acceptor_.async_accept(
-					net::make_strand(ioc_),
-					beast::bind_front_handler(
-						&listener::on_accept,
-						shared_from_this()));
+			// Bind to the server address
+			acceptor_.bind(endpoint, ec);
+			if (ec) {
+				fail(ec, "bind");
+				return;
 			}
 
-			void on_accept(beast::error_code ec, tcp::socket socket) {
-				if (ec) {
-					fail(ec, "accept");
-				}
-				else {
-					// Create the session and run it
-					std::make_shared<session>(
-						std::move(socket),
-						doc_root_)->run();
-				}
-
-				// Accept another connection
-				do_accept();
+			// Start listening for connections
+			acceptor_.listen(net::socket_base::max_listen_connections, ec);
+			if (ec) {
+				fail(ec, "listen");
+				return;
 			}
-		};
+		}
 
+		// Start accepting incoming connections
+		void run() {
+			do_accept();
+		}
+
+	private:
+		void do_accept() {
+			// The new connection gets its own strand
+			acceptor_.async_accept(
+				net::make_strand(ioc_),
+				beast::bind_front_handler(
+					&listener::on_accept,
+					shared_from_this()));
+		}
+
+		void on_accept(beast::error_code ec, tcp::socket socket) {
+			if (ec) {
+				fail(ec, "accept");
+			}
+			else {
+				// Create the session and run it
+				std::make_shared<session>(
+					std::move(socket),
+					doc_root_)->run();
+			}
+
+			// Accept another connection
+			do_accept();
+		}
+	};
+
+	//***************************************************************************
+	//*                    http_server_async
+	//***************************************************************************
 	inline int http_server_async(int argc, char* argv[]) {
 		// Check command line arguments.
 		if (argc != 5) {
